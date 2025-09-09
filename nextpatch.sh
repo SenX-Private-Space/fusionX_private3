5e4f80f50d1a2db3b8adbefcf84db53bfb23a653
SENX | センクス <senseixdev@gmail.com>
2025-09-09 11:36:26 +0000

    patch: Add Wild-KernelSU support
    Signed-off-by: SenX <senseiix@dev.com>

--- nextpatch.sh
+++ nextpatch.sh
@@ -1,3 +1,5 @@
 rm -rf KernelSU-Next
-curl -LSs "https://raw.githubusercontent.com/re-noroi/KernelSU-Next/next-susfs_v1.5.5-v1.5.7/kernel/setup.sh" | bash -s next-susfs_v1.5.5-v1.5.7
+curl -LSs "https://raw.githubusercontent.com/SenseiiX/KernelSU-Next/next-susfs_v1.5.5-v1.5.7/kernel/setup.sh" | bash -s next-susfs_v1.5.5-v1.5.7
+echo "Adding more manager support..."
+echo "Done (KSU, MKSU, RKSU, Fork by backslashxx, SukiSU and WKSU)"
 cp -rf "0001-KSUN-BPF.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "0001-KSUN-BPF.patch" && cd ..
